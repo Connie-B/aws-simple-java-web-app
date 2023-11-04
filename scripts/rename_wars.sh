307e8e0ac9c1eb2077cb6a3e66d372a3b64ba229
Connie Blaisdell <132613587+Connie-B@users.noreply.github.com>
2023-11-04 11:30:58 -0500

    Make this application the Root app

--- scripts/rename_wars.sh
+++ scripts/rename_wars.sh
@@ -7,12 +7,12 @@
 
 echo "-xxxxxxxxxx Running war renamer script xxxxxxxxxx-"
 
-# appspec.yml did deploy all war files to /opt/apache-tomcat-9.0.80/webapps during the Install event
+# appspec.yml did deploy our war file to /opt/apache-tomcat-9.0.80/webapps during the Install event
 cd /opt/apache-tomcat-9.0.80/webapps
 
-# rename the war file (remove everything after the first hyphen)  
-# so 'simpleJavaWebApp-1.0-SNAPSHOT.war' becomes 'simpleJavaWebApp.war'
-# for file in ./*.war ; do echo mv "$file" "${file%%-*}.war" ; done
-for file in ./*.war ; do mv "$file" "${file%%-*}.war" ; done
+# delete existing ROOT dir
+rm -rf ROOT
+# rename our war file so it will be the root application
+mv simpleJavaWebApp-1.0-SNAPSHOT.war ROOT.war
 
 echo "-xxxxxxxxxx End of war renamer script xxxxxxxxxx-"
